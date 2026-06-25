3b6b83bc9e1aff548ef861be03bd0b9a9affc656
Adarsh <mamidiadarshreddy@gmail.com>
2026-06-25 09:39:17 +0530

    Initial commit background color changer

---
+++ BACKGROUND-CHANGER/script.js
@@ -0,0 +1,34 @@
+const red=document.getElementById("red")
+const blue=document.getElementById("blue")
+const orange=document.getElementById("orange")
+const green=document.getElementById("green")
+const pink=document.getElementById("pink")
+const purple=document.getElementById("purple")
+const yellow=document.getElementById("yellow")
+const violet=document.getElementById("violet")
+
+
+red.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#c1121f";
+});
+blue.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#219ebc";
+});
+orange.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#fb8500";
+});
+green.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#a7c957";
+});
+pink.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#ffafcc";
+});
+purple.addEventListener("click", () => {
+    document.body.style.backgroundColor = "purple";
+});
+yellow.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#ffd60a";
+});
+violet.addEventListener("click", () => {
+    document.body.style.backgroundColor = "#8338ec";
+});
